16b4c8670a3fac8322bbf6d0a563fff5fa2e6e2b
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-10 14:11:56 -0700

    add result render
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,7 +5,8 @@ function Result(props) {
   return (
   <React.Fragment>
     <p>{result.sentence1} <span><strong>{result.pos1}</strong></span> {result.sentence2} <span><strong>{result.pos2}</strong></span> {result.sentence3} <span><strong>{result.pos3}</strong></span> {result.sentence4} <span><strong>{result.pos4}</strong></span> {result.sentence5} <span><strong>{result.pos5}</strong></span></p>
-  </React.Fragment>)
+  </React.Fragment>
+  )
 }
 
 export default Result;
