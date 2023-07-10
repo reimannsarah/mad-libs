d7936f20179ff84a0e045aba2907459f2a80bf82
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-10 11:55:28 -0700

    add result component
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

---
+++ src/components/Result.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Result(props) {
+  const result = props.result;
+  return (
+  <React.Fragment>
+    <p>{result.sentence1} <span><strong>{result.pos1}</strong></span> {result.sentence2} <span><strong>{result.pos2}</strong></span> {result.sentence3} <span><strong>{result.pos3}</strong></span> {result.sentence4} <span><strong>{result.pos4}</strong></span> {result.sentence5} <span><strong>{result.pos5}</strong></span></p>
+  </React.Fragment>)
+}
+
+export default Result;
