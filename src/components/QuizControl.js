9f98b998c8a77725da9e04461bf7c30869857b6c
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-10 11:55:11 -0700

    add function to handleQuizSubmission
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- src/components/QuizControl.js
+++ src/components/QuizControl.js
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import NewQuizForm from "./NewQuizForm";
 import QuizList from "./QuizList";
 import TakeQuiz from "./TakeQuiz";
+import Result from "./Result";
 
 function QuizControl() {
 	const [quizFormVisible, setQuizFormVisible] = useState(false);
   const [quizList, setQuizList] = useState([]);
+  const [resultList, setResultList] = useState([]);
+  const [result, setResult] = useState(null);
   const [selectedQuiz, setSelectedQuiz] = useState(null);
 
   const handleAddingNewQuiz = (newQuiz) => {
@@ -23,10 +26,19 @@ function QuizControl() {
     setSelectedQuiz(selection);
   }
 
+  const handleQuizSubmission = (result) => {
+    const newResultList = resultList.concat(result);
+    setResultList(newResultList);
+    setSelectedQuiz(null);
+    setResult(result);
+  }
+
   let currentlyVisible = null;
 
-  if (selectedQuiz !== null) {
-    currentlyVisible = <TakeQuiz selection={selectedQuiz} />
+  if (result !== null) {
+    currentlyVisible = <Result result={result} />
+  } else if (selectedQuiz !== null) {
+    currentlyVisible = <TakeQuiz selection={selectedQuiz} onQuizSubmission={handleQuizSubmission}/>
   } else if (quizFormVisible) {
     currentlyVisible = 
     <NewQuizForm onNewQuizCreation={handleAddingNewQuiz} />
