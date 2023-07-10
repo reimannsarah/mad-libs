d32a8536af6dff4f52cd9452497130f41a3905b3
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-10 11:16:44 -0700

    add function to select individual quiz

--- src/components/QuizControl.js
+++ src/components/QuizControl.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import NewQuizForm from "./NewQuizForm"
-// import QuizList from "./QuizList"
+import NewQuizForm from "./NewQuizForm";
+import QuizList from "./QuizList";
+import TakeQuiz from "./TakeQuiz";
 
 function QuizControl() {
 	const [quizFormVisible, setQuizFormVisible] = useState(false);
   const [quizList, setQuizList] = useState([]);
+  const [selectedQuiz, setSelectedQuiz] = useState(null);
 
   const handleAddingNewQuiz = (newQuiz) => {
     const newQuizList = quizList.concat(newQuiz);
@@ -16,14 +18,24 @@ function QuizControl() {
     setQuizFormVisible(!quizFormVisible);
   } 
 
+  const handleChangingQuiz = (id) => {
+    const selection = quizList.filter(quiz => quiz.id === id)[0];
+    setSelectedQuiz(selection);
+  }
+
   let currentlyVisible = null;
 
-  if (quizFormVisible) {
+  if (selectedQuiz !== null) {
+    currentlyVisible = <TakeQuiz selection={selectedQuiz} />
+  } else if (quizFormVisible) {
     currentlyVisible = 
     <NewQuizForm onNewQuizCreation={handleAddingNewQuiz} />
   } else {
     currentlyVisible = 
-    <h1>CRANK THE button OUT OF HERE</h1>
+    <React.Fragment>
+      <h1>Quizzes</h1>
+      <QuizList quizList={quizList} onQuizSelect={handleChangingQuiz} />
+    </React.Fragment>
   }
   return (
     <React.Fragment>
