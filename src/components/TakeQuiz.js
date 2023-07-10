5284b4250ed3b0d50df065bfa9f2c92f68e59184
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-10 11:56:21 -0700

    remove sentences from quiz

--- src/components/TakeQuiz.js
+++ src/components/TakeQuiz.js
@@ -1,23 +1,35 @@
 import React from "react";
+import { v4 } from "uuid";
 
 function TakeQuiz(props) {
   const quiz = props.selection;
 
-  // function handleQuizSubmit
+  function handleQuizSubmit(event) {
+    event.preventDefault();
+    props.onQuizSubmission({
+      id: v4(),
+      sentence1: quiz.sentence1,
+      pos1: event.target.pos1.value,
+      sentence2: quiz.sentence2,
+      pos2: event.target.pos2.value,
+      sentence3: quiz.sentence3,
+      pos3: event.target.pos3.value,
+      sentence4: quiz.sentence4,
+      pos4: event.target.pos4.value,
+      sentence5: quiz.sentence5,
+      pos5: event.target.pos5.value
+    })
+  }
+
   return (
     <React.Fragment>
       <h1>{quiz.title}</h1>
-      <form>
-        <p>{quiz.sentence1}</p>
-        <input type="text" placeholder={quiz.pos1} />
-        <p>{quiz.sentence2}</p>
-        <input type="text" placeholder={quiz.pos2} />
-        <p>{quiz.sentence3}</p>
-        <input type="text" placeholder={quiz.pos3} />
-        <p>{quiz.sentence4}</p>
-        <input type="text" placeholder={quiz.pos4} />
-        <p>{quiz.sentence5}</p>
-        <input type="text" placeholder={quiz.pos5} />
+      <form onSubmit={handleQuizSubmit}>
+        <input name="pos1" type="text" placeholder={quiz.pos1} />
+        <input name="pos2" type="text" placeholder={quiz.pos2} />
+        <input name="pos3" type="text" placeholder={quiz.pos3} />
+        <input name="pos4" type="text" placeholder={quiz.pos4} />
+        <input name="pos5" type="text" placeholder={quiz.pos5} />
 
         <button type="submit">See Madlib Result!</button>
       </form>
