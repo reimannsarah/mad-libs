7c6413ab779427e95d489c5c62c89f7df6427ffa
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-13 11:03:34 -0700

    remove unnecessary imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,5 @@
 import QuizControl from './QuizControl';
 import './App.css';
-import Header from "./Header.js"
-import SignIn from './SignIn';
-import SignOut from './SignOut';
 import React from 'react';
 
 function App() {
